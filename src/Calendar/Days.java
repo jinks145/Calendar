ce54c0b5154ae7d205621b399a34a95b88b5c49a
jinks145 <jinks14544@gmail.com>
2019-04-15 14:38:11 +0900

    링크드 리스트로 하여 접근성을 용이 하게 함

--- src/Calendar/Days.java
+++ src/Calendar/Days.java
@@ -1,34 +1,7 @@
 package Calendar;
-import java.util.*;
 
 public class Days {
-	int year[][] = new int[12][30];
-	LinkedList<Integer> years = new LinkedList<Integer>();
-	
-	public Days(int[][] arr, LinkedList<Integer> years) {
-		
-		this.year = arr;
-		this.years = years;
-	}
 
-	public int[][] getArr() {
-		return year;
-	}
-
-	public void setArr(int[][] arr) {
-		this.year = arr;
-	}
-
-	public LinkedList<Integer> getYears() {
-		return years;
-	}
-
-	public void setYears() {
-		
-				
-		
-		this.years = years;
-	}
 	
 	
 	
--- src/Years/Year.java
+++ src/Years/Year.java
@@ -1,5 +1,14 @@
 package Years;
 
 public class Year {
-
+		int month[][] = new int[12][30];
+		Year nextYear = null;
+		int year = 0;
+		
+		
+		public void setYear(int year) {
+			this.year = year;
+		}
+		
+		
 }
